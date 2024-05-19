89ca5a86eeb1523f002735441faed01a45e8caec
Gabe-Git-Go <114177782+Gabe-Git-Go@users.noreply.github.com>
2024-05-18 21:57:51 -0700

    New Chagnes

--- src/components/shared/footer.jsx
+++ src/components/shared/footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BsInstagram, BsLinkedin } from 'react-icons/bs';
+import { FaSoundcloud } from 'react-icons/fa';
+import { GrGithub } from 'react-icons/gr';
+
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
         <p>&copy; 2024 Gabriel Goshorn. All rights reserved.</p>
+        <div className='footer-social-links'>
+            <a href="https://www.instagram.com/g_a_b_e__g__b_a_b_y/"><BsInstagram/></a>
+            <a href="https://soundcloud.com/gabe-goshorn"><FaSoundcloud/></a>
+            <a href="https://www.linkedin.com/in/gabriel-goshorn/"><BsLinkedin/></a>
+            <a><GrGithub/></a>
+        </div>
+
       </div>
     </footer>
   );
