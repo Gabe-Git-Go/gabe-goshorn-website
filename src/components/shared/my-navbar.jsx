2710a9b333b6d6f5b79774cb41f0e2dbdc8c2a5d
Gabe-Git-Go <114177782+Gabe-Git-Go@users.noreply.github.com>
2024-05-19 15:11:37 -0700

    style changes

--- src/components/shared/my-navbar.jsx
+++ src/components/shared/my-navbar.jsx
@@ -5,11 +5,11 @@ function MyNavbar(){
   return (
     <div id="my-nav-bar-wrapper">
       <ul id="my-nav-bar-list">
-        <li><NavLink style={({ isActive }) => ({  
+        <li><NavLink className="nav-bar-item" style={({ isActive }) => ({  
                             color: isActive ? 'orange' : 'rgb(146 145 142)' })}  to="/">Home</NavLink></li>
-        <li><NavLink style={({ isActive }) => ({  
+        <li><NavLink className="nav-bar-item" style={({ isActive }) => ({  
                             color: isActive ? 'orange' : 'rgb(146 145 142)' })}  activeclassname="active" to="/music">Music</NavLink></li>
-        <li><NavLink style={({ isActive }) => ({  
+        <li><NavLink className="nav-bar-item" style={({ isActive }) => ({  
                             color: isActive ? 'orange' : 'rgb(146 145 142)' })}  activeclassname="active" to="/hire-me">Hire Me</NavLink></li>
       </ul>
     </div>
