a347eef7da633eb1c1bd8ea7b39c169326665a16
Gabe-Git-Go <114177782+Gabe-Git-Go@users.noreply.github.com>
2024-06-10 13:44:49 -0700

    Fix contact form validation and email service

--- src/services/hire-me/emailService.jsx
+++ src/services/hire-me/emailService.jsx
@@ -2,10 +2,11 @@ import { getHttpClient } from '../../shared/config/httpClient';
 
 
 export const sendEmail = async (emailDetails) => {
+    console.log(process.env.BE_API_BASE_URL)
     return await getHttpClient().post('/hire-me/email/send', emailDetails,
         {
             validateStatus: (status) => {
-                return status < 500
+                return status < 400
             }
         }
     )
--- src/shared/config/environment.jsx
+++ src/shared/config/environment.jsx
@@ -1,2 +1,18 @@
+//Change local to your local for your server
+export const getEnvironment = () => {
 
+    const environment = {
+        env: window.location.href.includes('local')? 'local':'prod',
+        API_BASE_URL: ''
+    }
+    switch (environment.env) {
+        case 'prod':
+          environment.API_BASE_URL = process.env.REACT_APP_API_BASE_URL
+          break;
+        default:
+          environment.API_BASE_URL = 'http://localhost:8080'
+    }
+
+    return environment;
+}
 
